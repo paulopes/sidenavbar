df2039fefdd5d8773b4c475b83c1b27f4477c124
Paulo Lopes <paulopes00@gmail.com>
2020-04-05 10:12:35 -0400

    Execute javascript only after the page is ready

--- sidenavbar.js
+++ sidenavbar.js
@@ -1,40 +1,42 @@
-var sidenavbarbehind = document.createElement('div')
-sidenavbarbehind.setAttribute('id', 'sidenavbarbehind')
-document.body.appendChild(sidenavbarbehind)
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenavbarbehind = document.createElement('div')
+    sidenavbarbehind.setAttribute('id', 'sidenavbarbehind')
+    document.body.appendChild(sidenavbarbehind)
 
-sidenavbarbehind.addEventListener('click', function(event) {
-    if (document.body.classList.contains('sidenavbar-active')) {
-        document.body.classList.remove('sidenavbar-active')
-    }
-})
+    sidenavbarbehind.addEventListener('click', function(event) {
+        if (document.body.classList.contains('sidenavbar-active')) {
+            document.body.classList.remove('sidenavbar-active')
+        }
+    })
 
-window.addEventListener(
-    'DOMContentLoaded',
-    function() {
-        document.body.querySelectorAll('.navbar').forEach(function(el) {
-            var sidenavbarbutton = document.createElement('div')
-            sidenavbarbutton.classList.add('sidenavbar-button')
+    window.addEventListener(
+        'DOMContentLoaded',
+        function() {
+            document.body.querySelectorAll('.navbar').forEach(function(el) {
+                var sidenavbarbutton = document.createElement('div')
+                sidenavbarbutton.classList.add('sidenavbar-button')
 
-            var sidenavbarbuttontop = document.createElement('div')
-            sidenavbarbuttontop.classList.add('sidenavbar-button-top')
-            sidenavbarbutton.appendChild(sidenavbarbuttontop)
-            var sidenavbarbuttonmid = document.createElement('div')
-            sidenavbarbuttonmid.classList.add('sidenavbar-button-mid')
-            sidenavbarbutton.appendChild(sidenavbarbuttonmid)
-            var sidenavbarbuttonbottom = document.createElement('div')
-            sidenavbarbuttonbottom.classList.add('sidenavbar-button-bottom')
-            sidenavbarbutton.appendChild(sidenavbarbuttonbottom)
+                var sidenavbarbuttontop = document.createElement('div')
+                sidenavbarbuttontop.classList.add('sidenavbar-button-top')
+                sidenavbarbutton.appendChild(sidenavbarbuttontop)
+                var sidenavbarbuttonmid = document.createElement('div')
+                sidenavbarbuttonmid.classList.add('sidenavbar-button-mid')
+                sidenavbarbutton.appendChild(sidenavbarbuttonmid)
+                var sidenavbarbuttonbottom = document.createElement('div')
+                sidenavbarbuttonbottom.classList.add('sidenavbar-button-bottom')
+                sidenavbarbutton.appendChild(sidenavbarbuttonbottom)
 
-            el.appendChild(sidenavbarbutton)
+                el.appendChild(sidenavbarbutton)
 
-            sidenavbarbutton.addEventListener('click', function(event) {
-                if (document.body.classList.contains('sidenavbar-active')) {
-                    document.body.classList.remove('sidenavbar-active')
-                } else {
-                    document.body.classList.add('sidenavbar-active')
-                }
+                sidenavbarbutton.addEventListener('click', function(event) {
+                    if (document.body.classList.contains('sidenavbar-active')) {
+                        document.body.classList.remove('sidenavbar-active')
+                    } else {
+                        document.body.classList.add('sidenavbar-active')
+                    }
+                })
             })
-        })
-    },
-    false,
-)
+        },
+        false,
+    )
+})
